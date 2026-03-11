ce08e623161f3332f35ddb7223628fde857cf224
Vimato <monst3k003@gmail.com>
2026-03-11 20:52:39 +0700

    Update Card component to use Offer type

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,40 +1,48 @@
+import {Offer} from '../types/offer.ts';
+
 type CardProps = {
-  photoSrc: string;
+  offer: Offer;
 }
 
-export default function Card({photoSrc}: CardProps) {
+export default function Card({offer}: CardProps) {
   return (
     <article className="cities__card place-card">
+      {offer.isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src={photoSrc} width="260" height="200" alt="Place image"/>
+          <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place image"/>
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;80</b>
+            <b className="place-card__price-value">&euro;{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button place-card__bookmark-button--active button"
+          <button
+            className={`place-card__bookmark-button ${offer.isFavorite ? 'place-card__bookmark-button--active' : ''} button`}
             type="button"
           >
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
-            <span className="visually-hidden">In bookmarks</span>
+            <span className="visually-hidden">{offer.isFavorite ? 'In' : 'To'} bookmarks</span>
           </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: '80%'}}></span>
+            <span style={{width: `${offer.rating * 20}%`}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Wood and stone place</a>
+          <a href="#">{offer.title}</a>
         </h2>
-        <p className="place-card__type">Room</p>
+        <p className="place-card__type">{offer.type}</p>
       </div>
     </article>
   );
