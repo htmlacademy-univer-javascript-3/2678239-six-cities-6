9d5d63c5825f9344929fabaeffb88fd66e28071f
Vimato <monst3k003@gmail.com>
2026-03-11 19:15:09 +0700

    Add Routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,10 @@
 import MainPage from '../pages/MainPage.tsx';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute} from '../const.ts';
+import PageNotFound from '../pages/PageNotFound.tsx';
+import FavoritesPage from '../pages/FavoritesPage.tsx';
+import LoginPage from '../pages/LoginPage.tsx';
+import OfferPage from '../pages/OfferPage.tsx';
 
 type AppProps = {
   placesCount: number;
@@ -6,6 +12,29 @@ type AppProps = {
 
 export default function App({placesCount}: AppProps) {
   return (
-    <MainPage placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage placesCount={placesCount}/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage/>}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage/>}
+        />
+        <Route
+          path={'*'}
+          element={<PageNotFound/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,10 @@
 export const DefaultSettings = {
   PlacesCount: 5,
 };
+
+export enum AppRoute {
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+  Root = '/',
+}
