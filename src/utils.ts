1a31ebc1b2bf2d498886635ff5c9b0db61102fc5
Vimato <monst3k003@gmail.com>
2026-03-19 15:07:45 +0700

    Create Reviews components

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
+import dayjs from 'dayjs';
+
 export function findItemById<T extends { id: string }>(array: T[], id: string): T | undefined {
   return array.find((el) => el.id === id);
+}
+
+export function humanizeDateDay(date: string) {
+  return date ? dayjs(date).format('YYYY-MM-DD') : '';
+}
+
+export function humanizeDateMonth(date: string) {
+  return date ? dayjs(date).format('MMMM YYYY') : '';
 }
