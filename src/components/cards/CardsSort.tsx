953bc0f158a79853ee4d3737c7e7cf2a2a2921ed
Vimato <monst3k003@gmail.com>
2026-03-29 20:58:04 +0700

    Update sorting component

--- src/components/cards/CardsSort.tsx
+++ src/components/cards/CardsSort.tsx
@@ -8,7 +8,7 @@ type CardSortProps = {
 
 export default function CardsSort({onClick}: CardSortProps) {
   const [selectedSort, setSelectedSort] = useState<SortTypes>(SortTypes.Popular);
-
+  const [isOpen, setIsOpen] = useState(false);
   const handleClick = (type: SortTypes) => {
     setSelectedSort(type);
     onClick(type);
@@ -17,13 +17,13 @@ export default function CardsSort({onClick}: CardSortProps) {
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
-                  Popular
+      <span className="places__sorting-type" tabIndex={0} onClick={() => setIsOpen(!isOpen)}>
+        {SortTypeNames.get(selectedSort)}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
+      <ul className={`places__options places__options--custom ${isOpen ? 'places__options--opened' : ''}`}>
         {Array.from(SortTypeNames.entries()).map(([type, title]) => (
           <li
             key={type}
