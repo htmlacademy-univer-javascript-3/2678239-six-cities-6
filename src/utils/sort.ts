ec322054c3c1c1ebad8107bf293c97c820361c00
Vimato <monst3k003@gmail.com>
2026-03-31 12:34:48 +0700

    Update sort util

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -2,15 +2,14 @@ import {Offer} from '../types/offer.ts';
 import {SortTypes} from '../types/sort.ts';
 
 export function sortOffers(offers: Offer[], sortType: SortTypes) {
-  const offersCopy = [...offers];
   switch (sortType) {
     case SortTypes.Popular:
       return offers;
     case SortTypes.PriceHighToLow:
-      return offersCopy.sort((a, b) => b.price - a.price);
+      return offers.toSorted((a, b) => b.price - a.price);
     case SortTypes.PriceLowToHigh:
-      return offersCopy.sort((a, b) => a.price - b.price);
+      return offers.toSorted((a, b) => a.price - b.price);
     case SortTypes.TopRated:
-      return offersCopy.sort((a, b) => b.rating - a.rating);
+      return offers.toSorted((a, b) => b.rating - a.rating);
   }
 }
