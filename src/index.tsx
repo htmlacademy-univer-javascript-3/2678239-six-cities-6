ee048d9f6520b4ecb3a6652a19137671840a4d8a
Vimato <monst3k003@gmail.com>
2026-04-05 13:30:46 +0700

    Do request for auth status on App start

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
 import {Provider} from 'react-redux';
 import {store} from './store';
+import {getAuthorizationStatusAction} from './store/apiActions.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+store.dispatch(getAuthorizationStatusAction());
 
 root.render(
   <React.StrictMode>
