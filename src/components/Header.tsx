d370ac267bf4a1dc2d2f093f5ea29efbf4594b86
Vimato <monst3k003@gmail.com>
2026-04-05 13:39:25 +0700

    Update Main page to use auth status

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import Logo from './Logo.tsx';
+import {useAppSelector} from '../hooks/store.ts';
+import {AppRoute, AuthorizationStatus} from '../types/app.ts';
+import {Link} from 'react-router-dom';
+
+export default function Header() {
+  const authorizationStatus = useAppSelector((store) => store.authorizationStatus);
+  const user = useAppSelector((store) => store.user);
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <Logo/>
+          </div>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              {authorizationStatus === AuthorizationStatus.Auth
+                ?
+                <>
+                  <li className="header__nav-item user">
+                    <a className="header__nav-link header__nav-link--profile" href="#">
+                      <div className="header__avatar-wrapper user__avatar-wrapper">
+                      </div>
+                      <span className="header__user-name user__name">{user?.email}</span>
+                      <span className="header__favorite-count">3</span>
+                    </a>
+                  </li>
+                  <li className="header__nav-item">
+                    <a className="header__nav-link" href="#">
+                      <span className="header__signout">Sign out</span>
+                    </a>
+                  </li>
+                </>
+                :
+                <li className="header__nav-item user">
+                  <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__login">Sign in</span>
+                  </Link>
+                </li>}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
